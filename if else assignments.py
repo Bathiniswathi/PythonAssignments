a3fa7832347b18393559e7dc58682291092f04ce
Bathiniswathi <153846720+Bathiniswathi@users.noreply.github.com>
2024-01-18 14:00:09 +0530

    Add files via upload

---
+++ if else assignments.py
@@ -0,0 +1,74 @@
+name=input("enter beer name to check availbilty:")
+name=name.lower()
+if name=="kingfisher":
+   print(name,"yes it is availble:")
+elif name=="bira":
+   print(name,"yes it is availble:")
+elif name=="budviser":
+   print(name,"yes it is availble:")
+elif name=="boom":
+   print(name,"yes it is availble:")  
+else:
+   print(name,"sorry it is not availble,plese try tommrrow again") 
+
+
+
+#conditional statements
+#if
+#eilf
+#else
+name=input("enter beer name to check availabilty:")
+name=name.lower()
+if name=="kingfisher":   
+   print(name,"yes it is available")
+elif name=="bira":
+   print(name,"yes it is availble")
+elif name=="budviser":
+   print(name,"yes it is availble")
+elif name=="boom":
+   print(name,"yes it is available")
+else:
+   print(name,"sorry it is not available,plese try tommorrow again")
+
+
+#pro to find biggest of 2  given numbers,user input:
+
+#get input from user
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+if a > b:
+  print("a, is biggest number")
+else:
+   print("b, is biggest number") 
+
+
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+if b > a:
+   print("a is biggest number:")
+else:
+   print("b is biggest number:")
+
+
+
+#program to print biggest of three number
+a=int(input("enter the first number:"))
+b=int(input("enter the second number"))
+c=int(input("enter the third number:"))
+if a > b and a > c:
+   print("a is biggest number:")
+elif b > a and b > c:
+   print("b is biggest number:")
+else:
+   print("c is biggest number:") 
+
+
+#program to check weathwr the given number is in between 1to100
+
+num=int(input("enter the first number:"))
+if num>=1 and num<=100:
+   print(num,"is in between1 to 100:")
+else:
+   print(num,"is in between 1 to 100:")
+  
+         
