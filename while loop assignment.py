7995033f375fe74a6b2de7d0b2f48daecb9db879
Bathiniswathi <153846720+Bathiniswathi@users.noreply.github.com>
2024-01-23 13:06:05 +0530

    Add files via upload

---
+++ while loop assignment.py
@@ -0,0 +1,9 @@
+password = input ("Enter your password")
+while password != "program":
+    print("wrong password,please try again")
+    password = input("Enter your password:")
+    userName = input("Enter your username:")
+while userName != "aman":
+    print("sorry wrong user name,try again")
+    userName = input("Enter your username:")
+    print("you have logged in successfully")        
